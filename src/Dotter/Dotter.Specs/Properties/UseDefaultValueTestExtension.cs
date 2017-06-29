96c9d01767e8a59437e1d8f9460d2e84ee613520
J. Preiss <joerg.preiss@abatplus.de>
2017-06-29 23:10:11 +0200

    Changes in Dotter.spec

---
+++ src/Dotter/Dotter.Specs/Properties/UseDefaultValueTestExtension.cs
@@ -0,0 +1,8 @@
+﻿// ReSharper disable once CheckNamespace
+namespace TestFx.Configuration
+{
+  [TestConfiguration]
+  public class UseDefaultValueTestExtension : IUseTestExtension<DefaultValueTestExtension>
+  {
+  }
+}
---
+++ src/Dotter/Dotter.Specs/Properties/UseResetInstanceFieldsTestExtension.cs
@@ -0,0 +1,8 @@
+﻿// ReSharper disable once CheckNamespace
+namespace TestFx.Configuration
+{
+  [TestConfiguration]
+  public class UseResetInstanceFieldsTestExtension : IUseTestExtension<ResetInstanceFieldsTestExtension>
+  {
+  }
+}
